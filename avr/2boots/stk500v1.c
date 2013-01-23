ecae926f61c16dd874a735f6d36e3eedbf2a5a4f
mharizanov <mharizanov@gmail.com>
2013-01-23 11:21:05 +0200

    Update avr/2boots/stk500v1.c
    prevent WDT reset on timeout, as this caused the mmc_updater to never be called and application started immediately

--- avr/2boots/stk500v1.c
+++ avr/2boots/stk500v1.c
@@ -180,8 +180,10 @@ static char getch(void)
 		/* HACKME:: here is a good place to count times*/
 		count++;
 		if (count > MAX_TIME_COUNT) { 
-			WDTCSR = _BV(WDE);
-	  		while (1); // 16 ms 
+			return;
+			// If we reset at timeout, the mmc_updater will never be called as the sketch is immediately started
+			//WDTCSR = _BV(WDE);
+	  		//while (1); // 16 ms 
 	  	}
 	}
 
@@ -194,8 +196,9 @@ static char getch(void)
 		/* HACKME:: here is a good place to count times*/
 		count++;
 		if (count > MAX_TIME_COUNT) {
-			WDTCSR = _BV(WDE);
-	  		while (1); // 16 ms
+			return;
+			//WDTCSR = _BV(WDE);
+	  		//while (1); // 16 ms
 	  	}
 	}
 	return UDR;
